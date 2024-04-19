f428fdd1d51d5da3f2e0206a63a4b73db4416b54
FredL33 <fredetfifi@hotmail.fr>
2024-04-19 13:00:19 +0200

    bug signup

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -31,7 +31,9 @@ exports.login = (req, res, next) => {
                    res.status(200).json({
                        userId: user._id,
                        token: jwt.sign(
-                        { userId}
+                        { userId: user._id},
+                        'RANDOM_TOKEN_SECRET',
+                        {expiresIn: '24h'}
                        )
                    });
                })
