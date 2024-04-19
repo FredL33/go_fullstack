3bf2c04dcc5ca11986dcbd7213fea69b376c3dad
FredL33 <fredetfifi@hotmail.fr>
2024-04-19 12:56:57 +0200

    bug signup

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');//sert à ce qu'un email = 1 utilisateur
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
